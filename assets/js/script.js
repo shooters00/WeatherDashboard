f108e300685d0abdb1f74c928932e3f7fcc67140
shooters00 <shooters0000@gmail.com>
2021-06-27 15:15:57 -0400

    Added getDays function.  Grabbed current weather.  Weather cards on one line.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,18 @@ var cityListEl = document.getElementById("cityList");
 var apiKey = "46861786b19faeb5c53a77361744c4d6";
 var weatherBase = "https://api.openweathermap.org";
 var savedInfo = [];
+var day;
+var nextDays = [];
 
 //var weatherCurrent = weatherBase + "/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
 //var weatherForecast = weatherBase + "/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + apiKey;
 
+//Get the date from moment.js
+function getDays() {
+    for (i=0; i < 6; i++) {
+        nextDays[i] = moment().add(i, 'days').format("M/DD/YYYY");
+    }
+}
 
 //Get content from local storage and display on screen
 function init() {
@@ -43,28 +51,50 @@ function getGeo(event) {
     var geoSpec = weatherBase + "/geo/1.0/direct?q=" + city + "&limit=1&appid=" + apiKey;
  
     fetch(geoSpec)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        var lat = data[0].lat;
-        var lon = data[0].lon;
-        
-        var newCityInfo = {
-            city,
-            lat,
-            lon
-        };
-        console.log(newCityInfo)
-        savedInfo.push(newCityInfo);
-        localStorage.setItem("savedInfo", JSON.stringify(savedInfo));
-        var btn = document.createElement("button");
-        btn.textContent = city;
-        btn.classList.add("saveBtn", "col-12");
-        cityListEl.appendChild(btn);
-        searchText.value = "";
-      });
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+            
+            var newCityInfo = {
+                city,
+                lat,
+                lon
+            };
+            console.log(newCityInfo)
+            savedInfo.push(newCityInfo);
+            localStorage.setItem("savedInfo", JSON.stringify(savedInfo));
+            var btn = document.createElement("button");
+            btn.textContent = city;
+            btn.classList.add("saveBtn", "col-12");
+            cityListEl.appendChild(btn);
+            searchText.value = "";
+            getCurrentWeather(newCityInfo);
+        });
+}
+
+function getCurrentWeather(cityInfo) {
+    var currentSpec = weatherBase + "/data/2.5/onecall?lat=" + cityInfo.lat + "&lon=" + cityInfo.lon + "&exclude=minutely,hourly,alerts&units=imperial&appid=" + apiKey;
+
+    fetch(currentSpec)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            cityInfo.city;
+            var hum = data.current.humidity;
+            var uvi = data.current.uvi;
+            var wind = data.current.wind_speed;
+            var tmp = data.current.temp;
+            var daysIcon = data.current.weather[0].icon;
+            var iconURL = "http://openweathermap.org/img/wn/"+daysIcon+"@2x.png";
+            console.log (cityInfo.city + " " + day + " " + hum + " " + uvi + " " + wind + " " + tmp + " " + iconURL);
+
+        })
 }
 
 
